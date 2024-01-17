8cef36e8bcf3723c5f098fddf9cdec35625de284
kartikeysinghCA <154873798+kartikeysinghCA@users.noreply.github.com>
2024-01-17 09:57:02 +0530

    Delete src directory

--- src/main/java/com/kartikey/APICURD/repository/EmployeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kartikey.APICURD.repository;
-
-import com.kartikey.APICURD.model.Employee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface EmployeeRepository extends JpaRepository<Employee, Long> {
-
-}
--- src/main/java/com/kartikey/APICURD/repository/TravelRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kartikey.APICURD.repository;
-
-import com.kartikey.APICURD.model.Travel;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TravelRepository extends JpaRepository<Travel, Long> {
-
-}
